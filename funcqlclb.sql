721d01bba96bb0cff51779b0c7ee7b42ddf112e5
NaiTi33 <130662495+NaiTi33@users.noreply.github.com>
2023-05-06 22:40:18 +0700

    Update funcqlclb.sql

--- funcqlclb.sql
+++ funcqlclb.sql
@@ -1,16 +1,15 @@
 use qlclb;
 -- Function tính tổng điểm có được trong giải đấu La Liga để xếp hạng
 DELIMITER $$
-create function FUNC_TONGDIEM(TenCLB varchar(30), NamBD int)
-returns int
-DETERMINISTIC
+create function FUNC_TONGDIEM(TCLB varchar(30), NamBD int)
+	returns int
+	deterministic
 begin
 	declare CLB varchar(10);
     declare GDAU varchar(10);
     declare TONG int;
-    select MaCLB into CLB from CLB where TenCLB=TenCLB;
+    select MaCLB into CLB from CLB where TenCLB=TCLB;
     select MaGD into GDAU from GIAIDAU where TenGD='La Liga' and year(NgBD)=NamBD;
-    set TONG=0;
     select sum(case 
 		when left(TySo, 1)>right(TySo, 1) then 3
         when left(TySo, 1)=right(TySo, 1) then 1
@@ -21,7 +20,7 @@ begin
 end$$
 DELIMITER ;
 -- kiểm tra
-set @TenCLB = 'barca', @NamBD=2017;
-select FUNC_TONGDIEM(@TenCLB, @NamBD);
+set @TenCLB = 'Real Sociedad', @NamBD=2017;
+select FUNC_TONGDIEM(@TenCLB, @NamBD) as 'Điểm';
 -- Xóa
 drop function FUNC_TONGDIEM;
