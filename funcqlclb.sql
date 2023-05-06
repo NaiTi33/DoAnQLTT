84cae97d7028c657b605314867b200e781370cc7
NaiTi33 <130662495+NaiTi33@users.noreply.github.com>
2023-05-06 21:12:12 +0700

    Add files via upload

---
+++ funcqlclb.sql
@@ -0,0 +1,27 @@
+use qlclb;
+-- Function tính tổng điểm có được trong giải đấu La Liga để xếp hạng
+DELIMITER $$
+create function FUNC_TONGDIEM(TenCLB varchar(30), NamBD int)
+returns int
+DETERMINISTIC
+begin
+	declare CLB varchar(10);
+    declare GDAU varchar(10);
+    declare TONG int;
+    select MaCLB into CLB from CLB where TenCLB=TenCLB;
+    select MaGD into GDAU from GIAIDAU where year(NgBD)=NamBD;
+    set TONG=0;
+    select sum(case 
+		when left(TySo, 1)>right(TySo, 1) then 3
+        when left(TySo, 1)=right(TySo, 1) then 1
+        else 0
+        end) into TONG
+    from TRANDAU where CLB_A=CLB and MaGD=GDAU;
+    return TONG;
+end$$
+DELIMITER ;
+-- kiểm tra
+set @TenCLB = 'barca', @NamBD=2017;
+select FUNC_TONGDIEM(@TenCLB, @NamBD);
+-- Xóa
+drop function FUNC_TONGDIEM;
