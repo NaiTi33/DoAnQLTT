30e38d28fb0616d563a4fc6aed2b28a67153c9c6
ptt2510 <130917808+ptt2510@users.noreply.github.com>
2023-05-25 21:45:32 +0700

    Update cursorqlclb.sql

--- cursorqlclb.sql
+++ cursorqlclb.sql
@@ -1,6 +1,9 @@
 -- Tạo thêm thuộc tính chỉ số lương vào nhân viên. Dựa vào thời gian trong hợp đồng, cập nhật chỉ số lương là 1.2 dối với nhân viên làm việc tính đến hiện tại là dưới 3 năm,1.8 là từ 3 đến 5 năm, 2.0 là trên 5 năm
 DELIMITER $$
-ALTER TABLE dbo.NHANVIEN ADD chisoluong FLOAT ;
+
+ALTER TABLE NHANVIEN ADD chisoluong FLOAT ;
+DECLARE CONTINUE HANDLER 
+FOR NOT FOUND SET finished = 1;
 DECLARE cur CURSOR FOR SELECT NgBD,NgKT,MaNV FROM dbo.HOPDONG ;
 DECLARE ngbd DATE;
 DECLARE ngkt DATE;
@@ -8,10 +11,12 @@ DECLARE mnv VARCHAR(10);
 OPEN cur;
 cur: LOOP
 FETCH cur INTO ngbd,ngkt,mnv;
+IF finished = 1 THEN 
+LEAVE cur;
+END IF;
 UPDATE dbo.NHANVIEN SET chisoluong= 1.2 WHERE TIMESTAMPDIFF(YEAR,ngbd,GETDATE()) < 3 AND ngkt>GETDATE() AND mnv=MaNV;
-UPDATE dbo.NHANVIEN SET chisoluong= 1.8 WHERE (TIMESTAMPDIFF(YEAR,ngbd,GETDATE()) BETWEEN 3 AND 5)  AND ngkt>GETDATE() AND @mnv=MaNV;
+UPDATE dbo.NHANVIEN SET chisoluong= 1.8 WHERE (TIMESTAMPDIFF(YEAR,ngbd,GETDATE()) BETWEEN 3 AND 5)  AND ngkt>GETDATE() AND mnv=MaNV;
 UPDATE dbo.NHANVIEN SET chisoluong= 2.0 WHERE TIMESTAMPDIFF(YEAR,ngbd,GETDATE()) > 5 AND ngkt>GETDATE() AND mnv=MaNV;
-FETCH NEXT FROM @cur INTO @ngbd,@ngkt,@mnv
 END LOOP cur;
 CLOSE cur;
 END$$
