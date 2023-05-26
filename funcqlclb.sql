c4544cee58b6c4f3cdff8482a639b18df06b82c7
NaiTi33 <130662495+NaiTi33@users.noreply.github.com>
2023-05-26 20:22:36 +0700

    Update funcqlclb.sql

--- funcqlclb.sql
+++ funcqlclb.sql
@@ -11,18 +11,36 @@ begin
     select MaCLB into CLB from CLB where TenCLB=TCLB;
     select MaGD into GDAU from GIAIDAU where TenGD='La Liga' and year(NgBD)=NamBD;
     select sum(case 
-		when left(TySo, 1)>right(TySo, 1) then 3
-        when left(TySo, 1)=right(TySo, 1) then 1
-        else 0
+		when left(TySo, 1)>right(TySo, 1) and CLB_A=CLB then 3
+        when left(TySo, 1)<right(TySo, 1) and CLB_A=CLB then 0
+        when left(TySo, 1)<right(TySo, 1) and CLB_B=CLB then 3
+        when left(TySo, 1)>right(TySo, 1) and CLB_B=CLB then 0
+        when left(TySo, 1)=right(TySo, 1) and (CLB_A=CLB or CLB_B=CLB) then 1
         end) into TONG
-    from TRANDAU where CLB_A=CLB and MaGD=GDAU;
+    from TRANDAU where MaGD=GDAU;
     return TONG;
 end$$
 DELIMITER ;
 -- kiểm tra
-set @TenCLB = 'Real Sociedad', @NamBD=2017;
+-- dữ liệu có sẵn
+-- ('lfp2122cano2410','2021-10-24 21:00', '1-2','barc','rema','cano','lfp2122'), +0
+-- ('lfp2122sabe2103','2022-03-21 22:00', '0-4', 'rema', 'barc', 'sabe','lfp2122'), +3
+-- dữ liệu giả thêm vào
+insert into TRANDAU values
+    ('lfp2122sevi2507', '2022-07-25 18:00', '0-1', 'sevi', 'barc', 'rasp', 'lfp2122'), -- +3
+    ('lfp2122barc1508', '2022-08-15 20:45', '2-1', 'barc', 'rebe', 'cano', 'lfp2122'), -- +3
+    ('lfp2122reso0509', '2022-09-05 18:00', '1-1', 'reso', 'barc', 'anoe', 'lfp2122'), -- +1
+    ('lfp2122barc2609', '2022-09-26 20:45', '0-1', 'barc', 'atma', 'cano', 'lfp2122'); -- +0
+set @TenCLB = 'Barcelona', @NamBD=2021;
 select FUNC_TONGDIEM(@TenCLB, @NamBD) as 'Điểm';
 -- Xóa
+delete from TRANDAU
+where MaTD in (
+    'lfp2122sevi2507',
+    'lfp2122barc1508',
+    'lfp2122reso0509',
+    'lfp2122barc2609'
+);
 drop function FUNC_TONGDIEM;
 
 
